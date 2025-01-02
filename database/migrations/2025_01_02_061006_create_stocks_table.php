c225c604f761e916548c81ce9478ff1ba5f2034e
ashan321 <87981761+ashan321@users.noreply.github.com>
2025-01-02 11:44:11 +0530

    update db tables add products and stocks tables

---
+++ database/migrations/2025_01_02_061006_create_stocks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->enum('condition', ['new', 'used'])->default('new');
+            $table->string('serial_number')->unique()->nullable();
+            $table->integer('quantity');
+            $table->decimal('selling_price', 10, 2);
+            $table->decimal('cost_price', 10, 2);
+            $table->string('color')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stocks');
+    }
+};
