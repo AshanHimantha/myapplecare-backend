31d7d5316a3dd6774602a61c00ce3feea9b5d27d
Ashan Himantha <ashanhimantha321@gmail.com>
2025-06-20 07:32:27 +0530

    update CORS configuration to restrict allowed origins and enable credentials support

--- config/cors.php
+++ config/cors.php
@@ -3,10 +3,10 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['*'],
-    'allowed_origins_patterns' => ["*"],
+    'allowed_origins' => ['http://localhost:3000','https://system.1000dtechnology.com'],
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 ];
