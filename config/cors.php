58da6111d799e71aa8e13785cf7d604e0e8d0998
Ashan Himantha <ashanhimantha321@gmail.com>
2025-06-21 20:07:08 +0530

    .htaccess and CORS configuration for improved security and routing

--- config/cors.php
+++ config/cors.php
@@ -1,12 +1,16 @@
 <?php
 
 return [
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000','https://system.1000dtechnology.com'],
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'http://127.0.0.1:3000',
+        'https://system.1000dtechnology.com'
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true,
+    'supports_credentials' => false,
 ];
